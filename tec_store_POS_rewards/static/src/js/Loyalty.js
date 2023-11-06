eba365b2da71bce010d0a6854849c9b720dd4fca
gecm-odoo <gecm@odoo.com>
2023-11-06 10:21:34 -0600

    [ADD] Add module to edit the amount on the gift card and eWallet.
    This module allows the user to input the amount to pay with gift card or eWallet. In the standard, if the gift card covers all the amount, the order is validated automatically. If the gift card does not cover all the amount, the total available amount will be used. This module lets you chose a lower amount to pay with the gift card.

---
+++ tec_store_POS_rewards/static/src/js/Loyalty.js
@@ -0,0 +1,53 @@
+odoo.define("tec_store_POS_rewards.TecStoreLoyalty", function (require) {
+  "use strict";
+
+  const { _t } = require("web.core");
+  const { Gui } = require("point_of_sale.Gui");
+  const { Order } = require("point_of_sale.models");
+  const Registries = require("point_of_sale.Registries");
+
+  const TecStoreLoyalty = (Order) =>
+    class extends Order {
+      async _activateCode(code) {
+        if ((await this._inputAmount()) !== true) {
+          return;
+        }
+        return super._activateCode(code);
+      }
+
+      async _inputAmount() {
+        const { confirmed, payload: amount } = await Gui.showPopup(
+          "TextInputPopup",
+          {
+            title: _t("Enter Reward Amount"),
+            startingValue: "",
+            placeholder: _t("Amount"),
+          }
+        );
+        if (confirmed && amount && amount > 0) {
+          this.amount = -amount;
+        } else {
+          this.amount = 0;
+          return "Amount is not valid";
+        }
+        return true;
+      }
+
+      _getRewardLineValuesDiscount(args) {
+        const values = super._getRewardLineValuesDiscount(args);
+        if (values) {
+          if (!this.amount) {
+            this._inputAmount();
+          }
+          if (values[0].price && values[0].price < this.amount) {
+            values[0].price = this.amount;
+          }
+          if (values[0].points_cost && values[0].points_cost > -this.amount) {
+            values[0].points_cost = -this.amount;
+          }
+        }
+        return values;
+      }
+    };
+  Registries.Model.extend(Order, TecStoreLoyalty);
+});
