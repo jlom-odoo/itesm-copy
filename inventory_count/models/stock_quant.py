aea88c591ab77392c82beadfae5d8b53aead5cc5
cogs-odoo <cogs@odoo.com>
2023-11-14 11:33:54 -0600

    [IMP] Code fix for new use cases: Apply button deleted from quant line view Delete user_id from stock quant line in order to remove empty lines New grouping of stock quant lines in count location by packaging_id

--- inventory_count/models/stock_quant.py
+++ inventory_count/models/stock_quant.py
@@ -8,20 +8,22 @@ class StockQuant(models.Model):
 
     def _apply_inventory(self):
         group_by = self.read_group([('location_id.is_count_location', '=', True), ('parent_location_id.usage', '!=', 'view')],
-                                   ['inventory_quantity', 'lot_id'], ['parent_location_id', 'product_id', 'lot_id'], lazy=False)
+                                   ['inventory_quantity', 'lot_id', 'package_id'], ['parent_location_id', 'product_id', 'lot_id', 'package_id'], lazy=False)
         vals_list = []
         for quant in group_by:
             vals_list.append({
                 'product_id': quant['product_id'][0],
                 'location_id': quant['parent_location_id'][0],
                 'inventory_quantity': quant['inventory_quantity'],
-                'lot_id': quant['lot_id'][0] if quant['lot_id'] != False else False
+                'lot_id': quant['lot_id'] and quant['lot_id'][0] or False,
+                'package_id': quant['package_id'] and quant['package_id'][0] or False
             })
         if vals_list:
             new_quants = super().create(vals_list)
             ungrouped_quants = self.filtered(lambda q: q.location_id.is_count_location == False or q.parent_location_id.usage == 'view')  # nopep8
             super(StockQuant, ungrouped_quants + new_quants)._apply_inventory()
             (self - ungrouped_quants).action_set_inventory_quantity_to_zero()
+            (self - ungrouped_quants).write({'user_id': False})
             new_quants.action_set_inventory_quantity_to_zero()
             (self + new_quants)._merge_quants()
         else:
