72b86a83420b8c09747f6d8450024b745db3dbec
gecm-odoo <gecm@odoo.com>
2024-02-27 12:27:19 -0600

    [FIX] Fixed merging module error.

--- itesm_stock/models/stock_quant.py
+++ itesm_stock/models/stock_quant.py
@@ -9,26 +9,25 @@ class StockQuant(models.Model):
     def _apply_inventory(self):
         group_by = self.read_group([('location_id.is_count_location', '=', True), ('parent_location_id.usage', '!=', 'view'), ('inventory_quantity_set','=',True)],
                                    ['inventory_quantity', 'lot_id', 'package_id'], ['parent_location_id', 'product_id', 'lot_id', 'package_id'], lazy=False)
-        if not group_by:
-            super(StockQuant, self).apply_inventory()
+        vals_list = []
+        for quant in group_by:
+            vals_list.append({
+                'product_id': quant['product_id'][0],
+                'location_id': quant['parent_location_id'][0],
+                'inventory_quantity': quant['inventory_quantity'],
+                'lot_id': quant['lot_id'] and quant['lot_id'][0] or False,
+                'package_id': quant['package_id'] and quant['package_id'][0] or False
+            })
+        if vals_list:
+            new_quants = super().create(vals_list)
+            ungrouped_quants = self.filtered(lambda q: q.location_id.is_count_location == False or q.parent_location_id.usage == 'view')  # nopep8
+            super(StockQuant, ungrouped_quants)._apply_inventory()
+            (self - ungrouped_quants).action_set_inventory_quantity_to_zero()
+            (self - ungrouped_quants).write({'user_id': False})
+            (self + new_quants)._merge_quants()
+            res = self.env['stock.quant'].search(
+                [('inventory_quantity', '!=', 0), ('inventory_quantity_set','=',False)])
+            res._compute_inventory_quantity_set()
+            res._compute_inventory_diff_quantity()
         else:
-            vals_list = []
-            for quant in group_by:
-                vals_list.append({
-                    'product_id': quant['product_id'][0],
-                    'location_id': quant['parent_location_id'][0],
-                    'inventory_quantity': quant['inventory_quantity'],
-                    'lot_id': quant['lot_id'] and quant['lot_id'][0] or False,
-                    'package_id': quant['package_id'] and quant['package_id'][0] or False
-                })
-            if vals_list:
-                new_quants = super().create(vals_list)
-                ungrouped_quants = self.filtered(lambda q: q.location_id.is_count_location == False or q.parent_location_id.usage == 'view')  # nopep8
-                super(StockQuant, ungrouped_quants)._apply_inventory()
-                (self - ungrouped_quants).action_set_inventory_quantity_to_zero()
-                (self - ungrouped_quants).write({'user_id': False})
-                (self + new_quants)._merge_quants()
-                res = self.env['stock.quant'].search(
-                    [('inventory_quantity', '!=', 0), ('inventory_quantity_set','=',False)])
-                res._compute_inventory_quantity_set()
-                res._compute_inventory_diff_quantity()
+            super(StockQuant, self)._apply_inventory()
