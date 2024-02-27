8a2dea2bbb0c938ab878833c3c961a570f916b95
Eduardo Villalpando Mello <euvm@odoo.com>
2024-02-27 07:16:13 -0600

    [ADD] Product: Assign reordering rule external ids Create reordering rules on create. Assign external ids on default_code change

---
+++ itesm_stock/models/__init__.py
@@ -0,0 +1,6 @@
+from . import product_product
+from . import product_template
+from . import stock_route
+from . import stock_location
+from . import stock_quant
+from . import stock_warehouse_orderpoint
---
+++ itesm_stock/models/stock_quant.py
@@ -0,0 +1,34 @@
+from odoo import fields, models
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    parent_location_id = fields.Many2one('stock.location', related='location_id.location_id', store=True)
+
+    def _apply_inventory(self):
+        group_by = self.read_group([('location_id.is_count_location', '=', True), ('parent_location_id.usage', '!=', 'view'), ('inventory_quantity_set','=',True)],
+                                   ['inventory_quantity', 'lot_id', 'package_id'], ['parent_location_id', 'product_id', 'lot_id', 'package_id'], lazy=False)
+        if not group_by:
+            super(StockQuant, self).apply_inventory()
+        else:
+            vals_list = []
+            for quant in group_by:
+                vals_list.append({
+                    'product_id': quant['product_id'][0],
+                    'location_id': quant['parent_location_id'][0],
+                    'inventory_quantity': quant['inventory_quantity'],
+                    'lot_id': quant['lot_id'] and quant['lot_id'][0] or False,
+                    'package_id': quant['package_id'] and quant['package_id'][0] or False
+                })
+            if vals_list:
+                new_quants = super().create(vals_list)
+                ungrouped_quants = self.filtered(lambda q: q.location_id.is_count_location == False or q.parent_location_id.usage == 'view')  # nopep8
+                super(StockQuant, ungrouped_quants)._apply_inventory()
+                (self - ungrouped_quants).action_set_inventory_quantity_to_zero()
+                (self - ungrouped_quants).write({'user_id': False})
+                (self + new_quants)._merge_quants()
+                res = self.env['stock.quant'].search(
+                    [('inventory_quantity', '!=', 0), ('inventory_quantity_set','=',False)])
+                res._compute_inventory_quantity_set()
+                res._compute_inventory_diff_quantity()
