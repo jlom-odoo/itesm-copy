9468b2bd47c252c73e4ddcbf757c578ad41650d3
cogs-odoo <cogs@odoo.com>
2024-03-12 13:30:43 -0600

    [FIX] ListController Patch

--- itesm_stock/static/src/js/itesm_stock_list.js
+++ itesm_stock/static/src/js/itesm_stock_list.js
@@ -1,15 +1,11 @@
 /** @odoo-module **/
-
-import { useService } from "@web/core/utils/hooks";
 import { ListController } from "@web/views/list/list_controller";
 
-export class ItesmStockListController extends ListController {
-  setup() {
-    super.setup();
-  }
+import {patch} from "@web/core/utils/patch";
+
+patch(ListController.prototype, "itesm_stock_list_controller", {
   onClickCreate() {
-    console.log("this.model.root.resModel", this.model.root.resModel)
     if (this.model.root.resModel !== "stock.warehouse.orderpoint")
       super.onClickCreate();
   }
-}
+})
